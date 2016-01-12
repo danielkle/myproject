e037df1fed632714520125e04cca879366553d0d
danielkle <danielkle10@gmail.com>
2016-01-12 16:05:49 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+# email server
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'zadyband@gmail.com'
+MAIL_PASSWORD = 'zad1zad1'
+
+# administrator list
+ADMINS = ['zadyband@gmail.com']
